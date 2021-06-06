418ea087b52615cc1a385050e4b799e9c73e21de
José Moreira <canasvieiras@hotmail.com.br>
2021-06-06 11:17:30 -0300

    atualização dependencias

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,9 @@
-
 'use strict';
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/user');
+const authService = require('../services/auth-service');
 
-router.post('/login/', controller.login);
 router.post('/authenticate', controller.authenticate);
 router.post('/refresh-token', authService.authorize, controller.refreshToken);
 router.post('/', controller.create);
